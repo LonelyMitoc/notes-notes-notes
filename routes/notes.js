964a22b239f2fe1b71973a51cf8eb73eff0223d9
Triglyceride <jamesmatsu@gmail.com>
2022-11-02 16:14:46 -0700

    update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -11,3 +11,54 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// GET route for a specific notes via id
+notes.get('/:notes_id', (req, res) => {
+  const notesId = req.params.notes_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((notes) => notes.notes_id === notesId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No notes with the input ID');
+    });
+});
+
+// DELETE route for a specific notes
+notes.delete('/:notes_id', (req, res) => {
+  const notesId = req.params.notes_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // create new array of all notes except the one from the URL
+      const result = json.filter((notes) => notes.notes_id !== notesId);
+
+      // Save array to the file system
+      writeToFile('./db/db.json', result);
+
+      // Respond to DELETE request
+      res.json(`Notes ${notesId} has been deleted 🗑️`);
+    });
+});
+
+// POST route for new notes
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { notesTitle, notes } = req.body;
+
+  if (req.body) {
+    const newNotes = {
+      notesTitle,
+      notes,
+      notes_id: uuidv4(),
+    };
+
+    readAndAppend(newNotes, './db/db.json');
+    res.json(`Notes added successfully`);
+  } else {
+    res.errored('Error in adding notes');
+  }
+});
+
+module.exports = notes;
